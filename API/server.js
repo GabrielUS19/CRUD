3558511f8ea1e497dc4e2ed6d6e7766547febeab
GabrielUS19 <gabrieluchoa2607@gmail.com>
2025-07-14 22:00:57 -0300

    Criei a rota de criação de usuário e a validação de email e nome de usuário

--- API/server.js
+++ API/server.js
@@ -7,28 +7,57 @@ const prisma = new PrismaClient();
 const app = express();
 app.use(express.json());
 
+// Create User
+
 app.post("/users", async (req, res) => {
+  const name = req.body.name?.trim();
+  const email = req.body.email?.trim().toLowerCase();
+  const password = req.body.password?.replace(/\s/g, "");
+
+  if (!name || !email || !password) {
+    return res.status(400).json("All fields are required");
+  }
+
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (!emailRegex.test(email)) {
+    return res.status(400).json("Invalid Email format");
+  }
+
+  const user = await prisma.user.findUnique({
+    where: { email },
+  });
+
+  if (user) {
+    return res.status(400).json(`This Email is already in use!`);
+  }
+
   await prisma.user.create({
     data: {
-      email: req.body.email,
-      name: req.body.name,
-      password: req.body.password,
+      email: email,
+      name: name,
+      password: password,
     },
   });
 
-  res.status(201).json(req.body);
+  res.status(201).json(`${name} Is now a user`);
 });
+
+// Update User
+
+// Delete Account
 
 app.delete("/users", async (req, res) => {
   await prisma.user.delete({
-    where: { id: 4 },
+    where: { email: "" },
   });
 
   res.send("Deleted");
 });
 
 app.get("/users", async (req, res) => {
-  res.send(names);
+  const name = await prisma.user.findMany();
+  res.send(name);
 });
 
 app.listen(3000);
