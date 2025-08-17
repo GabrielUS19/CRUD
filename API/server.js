59f58103074aecda15f5f1024f3d67d702fb4026
GabrielUS19 <gabrieluchoa2607@gmail.com>
2025-08-17 10:11:23 -0300

    feat: Add a error 404 route

--- API/server.js
+++ API/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import authRoute from "./routes/authRoute.js";
+import userRoute from "./routes/userRoute.js";
 
 const app = express();
 app.use(express.json());
@@ -7,5 +8,12 @@ app.use(express.json());
 // Create User
 
 app.use(authRoute);
+app.use(userRoute);
+
+// Error 404
+
+app.use((req, res) => {
+  res.status(404).json({ message: "Error 404: Page not found" });
+});
 
 app.listen(3000);
