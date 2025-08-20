3ce4bfa5a84cdc0b88c684a30cd67a28389a66a4
GabrielUS19 <gabrieluchoa2607@gmail.com>
2025-08-19 21:16:03 -0300

    feat: Add a delete account route

--- API/controllers/userController.js
+++ API/controllers/userController.js
@@ -1,29 +1,34 @@
 import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
 
 const prisma = new PrismaClient();
 
 // Update the profile info by ID
 
-const update_patch = async (req, res) => {
+const update_profile_patch = async (req, res) => {
   const userID = parseInt(req.params.id, 10);
 
   const newName = req.body.newName.trim() || "";
 
   if (!newName) {
-    res.status(400).json({ message: "All fields are required" });
+    return res.status(400).json({ message: "All fields are required" });
   }
 
   if (isNaN(userID)) {
-    res.status(400).json({ message: "Invalid ID format" });
+    return res.status(400).json({ message: "Invalid ID format" });
   }
+
+  // Search for the user in the database by ID
 
   const user = await prisma.user.findUnique({
     where: { id: userID },
   });
 
   if (!user) {
-    res.status(400).json({ message: "Invalid ID" });
+    return res.status(400).json({ message: "User not found" });
   }
+
+  // Update the user profile
 
   await prisma.user.update({
     where: { id: userID },
@@ -33,6 +38,45 @@ const update_patch = async (req, res) => {
   res.status(200).json({ message: "Username updated" });
 };
 
+// Route to delete the own account
+
+const delete_account_delete = async (req, res) => {
+  const userID = parseInt(req.params.id, 10);
+
+  const password = req.body.password || "";
+
+  if (!password) {
+    return res
+      .status(400)
+      .json({ message: "The password field can't be empty" });
+  }
+
+  if (isNaN(userID)) {
+    return res.status(400).json({ message: "Invalid ID format" });
+  }
+
+  const user = await prisma.user.findUnique({
+    where: { id: userID },
+  });
+
+  if (!user) {
+    return res.status(400).json({ message: "User not found" });
+  }
+
+  const validPassword = await bcrypt.compare(password, user.password);
+
+  if (!validPassword) {
+    return res.status(400).json({ message: "Incorrect password" });
+  }
+
+  await prisma.user.delete({
+    where: { id: userID },
+  });
+
+  res.status(200).json({ message: "Account deleted" });
+};
+
 export default {
-  update_patch,
+  update_profile_patch,
+  delete_account_delete,
 };
--- API/routes/userRoute.js
+++ API/routes/userRoute.js
@@ -3,6 +3,8 @@ import userController from "../controllers/userController.js";
 
 const router = Router();
 
-router.patch("/users/update/:id", userController.update_patch);
+router.patch("/users/update/:id", userController.update_profile_patch);
+
+router.delete("/users/delete/:id", userController.delete_account_delete);
 
 export default router;
